b689ce6e798383d74022afeaf86140ba08760da3
Imran-92 <mr.imran92@hotmail.com>
2023-03-29 19:05:46 +0100

    added css styling to nav and homepage

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import appstore from '../images/appstore.png';
 import playstore from '../images/playstore.png';
+import { Link } from "react-router-dom";
 
 function HomeContet() {
   return (
@@ -13,12 +14,19 @@ function HomeContet() {
           <div className="header-content mx-auto">
             <h1 className="mb-5">Welcome to Love Lounge!</h1>
             <h4>A place to find your Perfect Match.</h4>
-            <button className="btn"><a href="#download" className="btn btn-outline btn-xl js-scroll-trigger">Find Your Match...</a></button>
+            <button className="btn"><a className="btn btn-outline btn-xl js-scroll-trigger"><Link to="/QuestionForm">Find Your Match...</Link></a></button>
           </div>
     </div>
    </div>
   </div>
   </header>
+  <section className="howItWork text-center bg-light">
+    <div className="container howItworks">
+      <h2 className="mb-5">How it Works...</h2>
+      <h5 className="mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+      and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
+    </div>
+  </section>
   <section className="download bg-primary text-center" id="download">
     <div className="container">
       <div className="row">
@@ -31,13 +39,6 @@ function HomeContet() {
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  <section className="howItWork text-center bg-light">
-    <div className="container howItworks">
-      <h2 className="mb-5">How it Works...</h2>
-      <h5 className="mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-      and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
     </div>
   </section>
   </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../images/Logo.png"
 
 const Navbar = () => {
 
     return (
-        <nav className="links">
-            <ul className="nav justify-content-end">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+            <img className="logo navbar-brand" src={Logo} alt="logoImage"  />
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
                 <li className="nav-item">
                     <Link to="/">Home</Link>
                 </li>
@@ -14,10 +20,10 @@ const Navbar = () => {
                 </li>
 
                 <li className="nav-item">
-                    <Link to="/QuestionForm">QuestionForm</Link>
+                    <Link to="/login.js">Login</Link>
                 </li>
-
             </ul>
+        </div>
         </nav>
     );
 };
